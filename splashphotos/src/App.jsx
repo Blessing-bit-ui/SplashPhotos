469d29a6b402db78618222fe617ff8d0236b0c8f
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-08-18 07:15:27 -0700

    created the photoDetails

--- splashphotos/src/App.jsx
+++ splashphotos/src/App.jsx
@@ -5,10 +5,11 @@ import './App.css'
 
  
  const AccessKey = "j_hdSyXs_ZFOHr-p8vAG28GSv-E8Q5bhVvD9JweaTM4";
-
+ 
 export default function App(){
   const [photos, setPhotos] = useState([])
   const [query, setQuery] = useState("")
+  const [selected, setSelected] = useState(null)
   useEffect(function(){
     async function fetchPhotos(){
       const res = await fetch(
@@ -20,15 +21,20 @@ export default function App(){
     }
     fetchPhotos();
   }, [query])
+
+  function handlePhoto(id) {
+    setSelected(id)
+    console.log(id)
+  }
   return(
     <div className="w-11/12">
       <Form query={query} setQuery={setQuery}/>
-      <Photos photos={photos}/>
+      <Photos photos={photos} onPhoto={handlePhoto} />
+      <PhotoDetails selected={selected}/>
     </div>
   )
 }
-
-
+ 
 function Form({query, setQuery}){
   return(
     <div className="bg-purple-700 p-3 mr-2 ml-2 border rounded-lg">
@@ -46,27 +52,46 @@ function Form({query, setQuery}){
   )
 }
 
-
-function Photos({photos}){
+function Photos({photos, onPhoto}){
 return(
   <div className=" h-fit">
   {photos.map((photo)=>(
-    <Photo key={photo.id} photo={photo}/>
+    <Photo key={photo.id} photo={photo} onPhoto={onPhoto}/>
   ))
 
   }
   </div>
 )
 }
- function Photo({photo}){
+ function Photo({photo, onPhoto}){
   const photoDescription = photo.alt_description
     .replaceAll("-", " ")
     .toUpperCase(0)
   
 return(
-  <div className="w-[250px] flex gap-4 shadow-lg   ">
+  <div className="w-[250px] flex gap-4 shadow-lg" onClick={()=>onPhoto(photo.id)}>
     <img src={photo.urls.thumb} alt={photo.alt_description} className="w-[50px] border rounded-lg" />
   <p><span className="font-bold">Photo description : </span><span className='text-sm'><em>{photoDescription}</em></span></p>
   </div>
 )
- }
+}
+
+function PhotoDetails({selected}){
+const [photo, setPhoto] = useState({})
+useEffect(function() {
+    async function fetchPhoto() {
+      const res = await fetch(
+        `https://api.unsplash.com/photos/${selected}?client_id=${AccessKey}`
+      );
+      const data = await res.json();
+      console.log(data);
+      setPhoto(data);
+    }
+    fetchPhoto();
+  }, [selected] )
+return(
+  <div>
+    <p></p>
+  </div>
+)
+}
